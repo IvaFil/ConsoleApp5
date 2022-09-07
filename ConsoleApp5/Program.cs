f062b4e3087e5a57f55693698a5347ee912d3ea2
IvaFil <filipovichiv@yandex.ru>
2022-09-07 21:21:06 +0300

    Процедура по выводу элементов результирующего массива

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -26,6 +26,14 @@ namespace ConsoleApp5
                 }
             }
 
+            void PrintArray(string[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
 
         }
     }
